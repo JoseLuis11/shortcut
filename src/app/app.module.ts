4dcc9ba2b9d82abcdbafa5750efb68ac301287e0
JoseLuis <jluis.scooter11@hotmail.com>
2017-09-27 19:23:13 -0700

    angularfire imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+
+//angularfire
 import { AngularFireModule } from 'angularfire2'
-import { LoginPage, RegisterPage } from '../pages/index.pages'
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { firebaseConfig } from '../config/firebase.config'
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+//pages
+import { LoginPage, RegisterPage, MyApp, HomePage } from '../pages/index.pages'
+
 
 @NgModule({
   declarations: [
@@ -19,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/index.pages.ts
+++ src/pages/index.pages.ts
@@ -1,2 +1,4 @@
 export { RegisterPage } from './register/register';
 export { LoginPage } from './login/login';
+export { MyApp } from '../app/app.component';
+export { HomePage } from '../pages/home/home';
