2475d9f25283ad5b9ae7c092e48664a8b95741b5
JoseLuis <jluis.scooter11@hotmail.com>
2017-09-27 19:16:50 -0700

    firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { AngularFireModule } from 'angularfire2'
 import { LoginPage, RegisterPage } from '../pages/index.pages'
- 
+import { firebaseConfig } from '../config/firebase.config'
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -18,7 +18,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/config/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDwHjrZ6khUORcymkmxamL8f8k2oALRJ3M",
+    authDomain: "shortcut-38c47.firebaseapp.com",
+    databaseURL: "https://shortcut-38c47.firebaseio.com",
+    projectId: "shortcut-38c47",
+    storageBucket: "shortcut-38c47.appspot.com",
+    messagingSenderId: "557168135420"
+  };
